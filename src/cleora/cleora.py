3b6115f819ff49b5a946e01c9f1440af6b63e167
Piotr Pilis <piotr@pilis.pl>
2023-03-13 19:58:15 +0100

    Add chunking graph into number of induced subgraphs

---
+++ src/cleora/cleora.py
@@ -0,0 +1,46 @@
+import math
+from typing import Generator
+
+import networkx as nx
+import numpy as np
+
+
+class Cleora:
+    def __init__(self):
+        """Initialize the Cleora class with config"""
+        pass
+
+    def embed(self, graph: nx.Graph) -> np.ndarray:
+        """Embed a graph into a vector space"""
+        chunks = self._chunk_graph(graph)
+
+        for chunk in chunks:
+            pass
+
+    def _chunk_graph(
+        self, graph: nx.Graph, num_chunks: int = 1
+    ) -> Generator[nx.Graph, None, None]:
+        """Chunk a graph into subgraphs"""
+        if not num_chunks > 0:
+            raise ValueError("num_chunks must be greater than 0")
+        # Determine the number of nodes in the graph
+        num_nodes = len(graph.nodes())
+        # Determine the size of each chunk
+        # If the number of nodes is not evenly divisible by the number of chunks, the last chunk will be smaller
+        chunk_size = math.ceil(num_nodes / num_chunks)
+
+        # Generate a subgraphs
+        for i in range(num_chunks):
+            start = i * chunk_size
+            end = start + chunk_size
+            if i == num_chunks - 1:
+                end = num_nodes
+            nodes = list(graph.nodes())[start:end]
+            # Add all neighbors of the nodes in the chunk to the chunk
+            neighbors = []
+            for node in nodes:
+                neighbors.extend(list(graph.neighbors(node)))
+            nodes.extend(neighbors)
+            # Get induced subgraph
+            subgraph = graph.subgraph(nodes)
+            yield subgraph
--- src/main.py
+++ src/main.py
@@ -1,14 +1,35 @@
 import logging
 
 import click
+import networkx as nx
+
+from cleora.cleora import Cleora
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
+def load_networkx_graph_from_file(filepath) -> nx.Graph:
+    """Load a networkx graph from a file"""
+    logger.info("Loading graph from file")
+    graph = nx.read_edgelist(filepath, nodetype=int)
+    logger.info("Graph loaded")
+    return graph
+
+
 @click.command()
-def main():
-    print("Hello, World!")
+@click.option(
+    "--filepath",
+    default="data/example_1.txt",
+    help="Path to the file containing the graph",
+)
+def main(filepath):
+    graph = load_networkx_graph_from_file(filepath)
+
+    cleora = Cleora()
+    embedding = cleora.embed(graph)
+
+    logger.info("Embedding: %s", embedding)
 
 
 if __name__ == "__main__":
